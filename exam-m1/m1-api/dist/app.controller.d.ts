8199b786b82c9d9f629aa025c95c193a26b073c0
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2024-11-08 21:43:14 +0100

    Delete exam-m1/m1-api/dist directory

--- exam-m1/m1-api/dist/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from './app.service';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    greetMe(name: string): Promise<string>;
-    getHello(): Promise<string>;
-}

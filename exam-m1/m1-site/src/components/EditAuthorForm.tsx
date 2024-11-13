f01b2500e4b0701519bd855fef609ad8f171844b
Loridan Juliane <juliane.loridan@student.junia.com>
2024-11-13 12:55:58 +0100

    update EditAuthorForm.tsx

--- exam-m1/m1-site/src/components/EditAuthorForm.tsx
+++ exam-m1/m1-site/src/components/EditAuthorForm.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { AuthorModel } from '../../../m1-api/src/modules/authors/author.model';
 function EditAuthorForm({  author, onClose }: { author: AuthorModel; onClose: () => void }) {
-  const [name, setName] = useState(author.firstName);
+  const [firstName, setfirstName] = useState(author.firstName);
+  const [lastName, setlastName] = useState(author.lastName);
   const [biography, setBiography] = useState(author.biography);
-  const [photo, setPhoto] = useState(author.image_path);
+  const [image_path, setimage_path] = useState(author.image_path);
 
   const handleSubmit = () => {
     fetch(`http://localhost:3001/authors/${author.id}`, {
       method: 'PATCH',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, biography, photo }),
+      body: JSON.stringify({ firstName, lastName, biography, image_path }),
     })
       .then(response => response.json())
       .then(() => onClose())
@@ -18,14 +19,17 @@ function EditAuthorForm({  author, onClose }: { author: AuthorModel; onClose: ()
 
   return (
     <form onSubmit={handleSubmit}>
+      <label>Prénom:
+        <input value={firstName} onChange={(e) => setfirstName(e.target.value)} />
+      </label>
       <label>Nom:
-        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <input value={lastName} onChange={(e) => setlastName(e.target.value)} />
       </label>
       <label>Biographie:
         <textarea value={biography} onChange={(e) => setBiography(e.target.value)} />
       </label>
       <label>URL de la photo:
-        <input value={photo} onChange={(e) => setPhoto(e.target.value)} />
+        <input value={image_path} onChange={(e) => setimage_path(e.target.value)} />
       </label>
       <button type="submit">Sauvegarder</button>
       <button onClick={onClose}>Annuler</button>
